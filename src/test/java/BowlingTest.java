211d6ce1bb43be201032086addd850075eb4aac8
JeanChambre <74455602+JeanChambre@users.noreply.github.com>
2021-09-11 01:02:39 +0200

    First Commit

---
+++ src/test/java/BowlingTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class BowlingTest {
+
+    @Test
+    public void scoringOne(){
+        int score = 0;
+        score =+ 1;
+        assertEquals(1,score);
+    }
+}
